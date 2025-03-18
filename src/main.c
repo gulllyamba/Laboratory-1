465f29e649b1072af7ee631db2ae0ab55dd3367f
gulllyamba <gulya.05.2006@icloud.com>
2025-03-19 01:37:52 +0300

    macros commit
    Added several macros for ease of use, also in main.c shows the work with COMPLEX_INT

--- src/main.c
+++ src/main.c
@@ -1,12 +1,11 @@
 #include "square_matrix.h"
 
-// Использование библиотеки на примере типа INT
-
 int main() {
+    // Использование библиотеки на примере типа INT
     int SIZE;
     scanf("%d", &SIZE); // Ввод размера матриц
 
-    SquareMatrix_t* A = create_matrix(SIZE, INT); // Инициализация матрицы A
+    SQUARE_MATRIX(A, SIZE, INT); // Инициализация матрицы A
     for (int i = 0; i < A->size; i++) { // Заполнение элементов матрицы A
         for (int j = 0; j < A->size; j++) {
             int x;
@@ -14,32 +13,32 @@ int main() {
             set_element(A, i, j, &x);
         }
     }
-    char* A_string = toString_matrix(A); // Перевод матрицы в строку
+    TO_STRING(A_string, A); // Перевод матрицы в строку
     printf("%s\n", A_string); // Вывод строки
 
-    SquareMatrix_t* B = create_matrix(SIZE, INT); // Инициализация матрицы B
+    SQUARE_MATRIX(B, SIZE, INT); // Инициализация матрицы B
     for (int i = 0; i < B->size; i++) { // Заполнение элементов матрицы B
         for (int j = 0; j < B->size; j++) {
             int x;
             scanf("%d", &x);
-            set_element(B, i, j, &x);
+            set_element(B, i, j, &x); // Установка элемента x на [i][j] место
         }
     }
-    char* B_string = toString_matrix(B);
+    TO_STRING(B_string, B);
     printf("%s\n", B_string);
 
-    SquareMatrix_t* SUMM = sum_matrix(A, B); // Матрица SUMM - сумма матриц A и B
-    char* SUMM_string = toString_matrix(SUMM);
+    SUMM_MATRICES(SUMM, A, B); // Матрица SUMM - сумма матриц A и B
+    TO_STRING(SUMM_string, SUMM);
     printf("%s\n", SUMM_string);
 
-    SquareMatrix_t* MULT = multiply_matrix(A, B); // Матрица MULT - произведение матриц A и B
-    char* MULT_string = toString_matrix(MULT); 
+    MULTIPLY_MATRICES(MULT, A, B); // Матрица MULT - произведение матриц A и B
+    TO_STRING(MULT_string, MULT);
     printf("%s\n", MULT_string);
 
     int scalar;
     scanf("%d", &scalar); // Ввод значения для скаляра
-    SquareMatrix_t* MULT_SC = multiply_matrix_scalar(A, &scalar); // Матрица MULT_SC - умножение матрицы A на скаляр (в данном случае - 5)
-    char* MULT_SC_string = toString_matrix(MULT_SC);
+    MULTIPLY_SCALAR_MATRICES(MULT_SC, A, &scalar); // Матрица MULT_SC - умножение матрицы A на скаляр (в данном случае - 5)
+    TO_STRING(MULT_SC_string, MULT_SC);
     printf("%s\n", MULT_SC_string);
 
     int row;
@@ -48,8 +47,8 @@ int main() {
     for (int i = 0; i < A->size - 1; i++) { // Заполнение массива коэффициентов
         scanf("%d", &alhpas[i]);
     }
-    SquareMatrix_t* LIN = linear_comb(A, row, (void*)alhpas); // Матрица SUMM - сумма матриц A и B
-    char* LIN_string = toString_matrix(LIN);
+    LINEAR_COMB(LIN, A, row, (void*)alhpas);// Матрица SUMM - сумма матриц A и B
+    TO_STRING(LIN_string, LIN);
     printf("%s\n", LIN_string);
 
     free(A_string); // освобождение памяти, занятой под строки
@@ -65,4 +64,73 @@ int main() {
     free_matrix(MULT);
     free_matrix(MULT_SC);
     free_matrix(LIN);
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Использование библиотеки на примере типа COMPLEX_INT
+    scanf("%d", &SIZE); // Ввод размера матриц
+
+    SQUARE_MATRIX(A_CMP, SIZE, COMPLEX_INT); // Инициализация матрицы A
+    for (int i = 0; i < A_CMP->size; i++) { // Заполнение элементов матрицы A
+        for (int j = 0; j < A_CMP->size; j++) {
+            int Re, Im;
+            scanf("%d %d", &Re, &Im);
+            COMPLEX_INT(value_A, &Re, &Im); // Создание комплексного числа
+            set_element(A_CMP, i, j, (void*)value_A); // Установка комплексного числа value на [i][j] место
+            free_complex_int(value_A); // Освобождение памяти, занятой под комплексное число
+        }
+    }
+    TO_STRING(A_CMP_string, A_CMP); // Перевод матрицы в строку
+    printf("%s\n", A_CMP_string); // Вывод строки
+
+    SQUARE_MATRIX(B_CMP, SIZE, COMPLEX_INT); // Инициализация матрицы B
+    for (int i = 0; i < B_CMP->size; i++) { // Заполнение элементов матрицы B
+        for (int j = 0; j < B_CMP->size; j++) {
+            int Re, Im;
+            scanf("%d %d", &Re, &Im);
+            COMPLEX_INT(value_B, &Re, &Im);
+            set_element(B_CMP, i, j, (void*)value_B);
+            free_complex_int(value_B);
+        }
+    }
+    TO_STRING(B_CMP_string, B_CMP);
+    printf("%s\n", B_CMP_string);
+
+    SUMM_MATRICES(SUMM_CMP, A_CMP, B_CMP); // Матрица SUMM - сумма матриц A и B
+    TO_STRING(SUMM_CMP_string, SUMM_CMP);
+    printf("%s\n", SUMM_CMP_string);
+
+    MULTIPLY_MATRICES(MULT_CMP, A_CMP, B_CMP); // Матрица MULT - произведение матриц A и B
+    TO_STRING(MULT_CMP_string, MULT_CMP);
+    printf("%s\n", MULT_CMP_string);
+
+    int scalar_CMP;
+    scanf("%d", &scalar_CMP); // Ввод значения для скаляра
+    MULTIPLY_SCALAR_MATRICES(MULT_SC_CMP, A_CMP, &scalar_CMP); // Матрица MULT_SC - умножение матрицы A на скаляр (в данном случае - 5)
+    TO_STRING(MULT_SC_CMP_string, MULT_SC_CMP);
+    printf("%s\n", MULT_SC_CMP_string);
+
+    int row_CMP;
+    scanf("%d", &row_CMP); // Ввод строки, к которой будет прибавляться линейная комбинация остальных строк
+    int* alhpas_CMP = (int*)calloc((A_CMP->size - 1), A_CMP->type->size); // Выделение памяти под массив коэффициентов для линейной комбинации и инициализация нулями
+    for (int i = 0; i < A_CMP->size - 1; i++) { // Заполнение массива коэффициентов
+        scanf("%d", &alhpas_CMP[i]);
+    }
+    LINEAR_COMB(LIN_CMP, A_CMP, row_CMP, (void*)alhpas_CMP);// Матрица SUMM - сумма матриц A и B
+    TO_STRING(LIN_CMP_string, LIN_CMP);
+    printf("%s\n", LIN_CMP_string);
+
+    free(A_CMP_string); // Освобождение памяти, занятой под строки
+    free(B_CMP_string);
+    free(SUMM_CMP_string);
+    free(MULT_CMP_string);
+    free(MULT_SC_CMP_string);
+    free(LIN_CMP_string);
+
+    free_matrix(A_CMP); // Освобождение памяти, занятой под матрицы
+    free_matrix(B_CMP);
+    free_matrix(SUMM_CMP);
+    free_matrix(MULT_CMP);
+    free_matrix(MULT_SC_CMP);
+    free_matrix(LIN_CMP);
 }
--- src/square_matrix.h
+++ src/square_matrix.h
@@ -12,6 +12,17 @@
 #include "types/COMPLEX/COMPLEX_INT.h"
 #include "types/COMPLEX/COMPLEX_DOUBLE.h"
 
+#define SQUARE_MATRIX(matrix_name, size, type) SquareMatrix_t* matrix_name = create_matrix(size, type)
+#define SUMM_MATRICES(matrix_name, matrix_a, matrix_b) SquareMatrix_t* matrix_name = sum_matrix(matrix_a, matrix_b)
+#define MULTIPLY_MATRICES(matrix_name, matrix_a, matrix_b) SquareMatrix_t* matrix_name = multiply_matrix(matrix_a, matrix_b)
+#define MULTIPLY_SCALAR_MATRICES(matrix_name, matrix_a, scalar) SquareMatrix_t* matrix_name = multiply_matrix_scalar(matrix_a, scalar)
+#define LINEAR_COMB(matrix_name, matrix_a, row, coef) SquareMatrix_t* matrix_name = linear_comb(matrix_a, row, coef)
+
+#define COMPLEX_INT(value_name, Re, Im) Complex_int_t* value_name = create_complex_int(Re, Im)
+#define COMPLEX_DOUBLE(value_name, Re, Im) Complex_double_t* value_name = create_complex_double(Re, Im)
+
+#define TO_STRING(string, matrix_name) char* string = toString_matrix(matrix_name);
+
 typedef struct SquareMatrix {
     int size;
     void* data;
